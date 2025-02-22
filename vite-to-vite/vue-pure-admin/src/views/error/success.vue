218cc40f6a7e26ae243d4d9346dd5e2308b81f76
wangyuhui <1272265541@qq.com>
2025-02-22 23:40:26 +0800

    [update]vite-to-vite中vue-pure-admin支持使用naive-ui-admin中的success组件

---
+++ vite-to-vite/vue-pure-admin/src/views/error/success.vue
@@ -0,0 +1,17 @@
+<script setup lang="ts">
+import { useRouter } from "vue-router";
+import { defineAsyncComponent } from "vue";
+
+defineOptions({
+  name: "403"
+});
+
+const router = useRouter();
+const remoteSuccess = defineAsyncComponent(() => import("naive-ui-admin/Success"));
+</script>
+
+<template>
+  <div class="flex justify-center items-center h-[640px]">
+    <remoteSuccess />
+  </div>
+</template>
